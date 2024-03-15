e669d97c79fe8810424d036b88647e8dc69765d4
sk1982 <sk1982@noreply.gitea.tendokyu.moe>
2024-03-14 20:23:19 -0400

    improve font performance

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,12 +8,12 @@ export default async function RootLayout({children}: LayoutProps) {
 		<html lang="en" className="h-full dark">
 			<head>
 				<title>Actaeon</title>
-				<link rel="preload" href={getAssetUrl('/fonts/rodinl.woff2')} as="font" type="font/woff2" crossOrigin="anonymous" />
-				<link rel="preload" href={getAssetUrl('/fonts/rodinm.woff2')} as="font" type="font/woff2" crossOrigin="anonymous" />
-				<link rel="preload" href={getAssetUrl('/fonts/rodindb.woff2')} as="font" type="font/woff2" crossOrigin="anonymous" />
-				<link rel="preload" href={getAssetUrl('/fonts/rodinb.woff2')} as="font" type="font/woff2" crossOrigin="anonymous" />
-				<link rel="preload" href={getAssetUrl('/fonts/rodineb.woff2')} as="font" type="font/woff2" crossOrigin="anonymous" />
-				<link rel="preload" href={getAssetUrl('/fonts/rodinub.woff2')} as="font" type="font/woff2" crossOrigin="anonymous" />
+				<link rel="preload" href={getAssetUrl('/fonts/FOT-RodinProN-L-en.woff2')} as="font" type="font/woff2" crossOrigin="anonymous" />
+				<link rel="preload" href={getAssetUrl('/fonts/FOT-RodinProN-M-en.woff2')} as="font" type="font/woff2" crossOrigin="anonymous" />
+				<link rel="preload" href={getAssetUrl('/fonts/FOT-RodinProN-DB-en.woff2')} as="font" type="font/woff2" crossOrigin="anonymous" />
+				<link rel="preload" href={getAssetUrl('/fonts/FOT-RodinProN-B-en.woff2')} as="font" type="font/woff2" crossOrigin="anonymous" />
+				<link rel="preload" href={getAssetUrl('/fonts/FOT-RodinProN-EB-en.woff2')} as="font" type="font/woff2" crossOrigin="anonymous" />
+				<link rel="preload" href={getAssetUrl('/fonts/FOT-RodinProN-UB-en.woff2')} as="font" type="font/woff2" crossOrigin="anonymous" />
 			</head>
 				<body className="h-full">
 				<Providers>
